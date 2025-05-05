286537ad7b7af7ddc47a3ea73e6cf1fefaed7ef7
Julio Raynier Castro Vargas <raynier97@hotmail.com>
2025-05-05 15:06:49 +0200

    Enhance index.php with improved styling and navigation. Add a header for UBL file upload and a link to view uploaded documents. Update form layout for better user experience.

--- index.php
+++ index.php
@@ -4,15 +4,40 @@
 <head>
     <title>UBL Parser</title>
     <link rel="stylesheet" href="assets/dropzone/dropzone.css" />
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        form {
+            margin-bottom: 20px;
+        }
+        .navigation {
+            margin-top: 20px;
+        }
+        a {
+            text-decoration: none;
+            color: #0066cc;
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
 </head>
 <body>
+    <h1>UBL File Upload</h1>
+    
     <!-- <form action="upload.php" class="dropzone" id="ublDropzone"></form> -->
 
     <form action="upload.php" method="post" enctype="multipart/form-data">
-    Select UBL / XML file to upload:
-    <input type="file" name="fileToUpload[]" id="fileToUpload" multiple>
-    <input type="submit" value="Upload UBL / XML" name="submit">
+        Select UBL / XML file to upload:
+        <input type="file" name="fileToUpload[]" id="fileToUpload" multiple>
+        <input type="submit" value="Upload UBL / XML" name="submit">
     </form>
+
+    <div class="navigation">
+        <a href="files_list.php">View Uploaded Documents</a>
+    </div>
 
     <script src="assets/dropzone/dropzone.js"></script>
 </body>
